acd28eb648165449182c4bfc39a71bfe1b867d0a
Nathaniel Felsen <n@medium.com>
2016-09-11 22:01:41 -0700

    Helloworld application

---
+++ test/helloworld_test.js
@@ -0,0 +1,18 @@
+var app = require('../helloworld');
+var Browser = require('zombie');
+var assert = require('assert');
+
+describe('main page', function() {
+  before(function() {
+    this.browser = new Browser({ site: 'http://localhost:3000' });
+  });
+
+  before(function(done) {
+    this.browser.visit('/', done);
+  });
+
+  it('should say hello world', function() {
+    assert.ok(this.browser.success);
+    assert.equal(this.browser.text(), "Hello World");
+  })
+});
