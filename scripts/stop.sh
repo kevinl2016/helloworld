8ae51dfa63b7a69c9f18522b4c057e7be39e6dab
Nathaniel Felsen <n@medium.com>
2016-09-18 22:23:35 -0700

    validation

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -2,4 +2,4 @@
 
 [[ -e /etc/init/helloworld.conf ]] && \
   status helloworld | grep -q '^helloworld start/running, process' && \
-    [[ $? -eq 0 ]] && stop helloworld
+    [[ $? -eq 0 ]] && stop helloworld || echo "Application not started"
