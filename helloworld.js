ff1ec26a24eb6d4684ce5dc7a315209d75cd72a5
Nathaniel Felsen <n@medium.com>
2017-06-03 11:56:46 -0700

    Adding winston to helloworld

--- helloworld.js
+++ helloworld.js
@@ -1,15 +1,33 @@
-var http = require("http");
+var http = require("http")
+var winston = require("Winston")
+
+var version = process.env.HELLOWORLD_VERSION
+
+var logger = new winston.Logger({
+  transports: [new winston.transports.Console({
+    timestamp: function() {
+       var d = new Date();
+       return d.toISOString();
+    },
+  })]
+})
+
+logger.rewriters.push(function(level, msg, meta) {
+  meta.version = version
+  return meta
+})
 
 http.createServer(function (request, response) {
 
    // Send the HTTP header
    // HTTP Status: 200 : OK
    // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.writeHead(200, {"Content-Type": "text/plain"})
 
    // Send the response body as "Hello World"
-   response.end('Hello World\n');
-}).listen(3000);
+   response.end("Hello World\n")
+}).listen(3000)
 
 // Console will print the message
-console.log('Server running');
+logger.info("Server running")
+
